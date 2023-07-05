44de49066a67bd07bd5f3a279ce6f8321bff570a
Sonica <sonicap2020@gmail.com>
2023-07-05 20:51:30 +0000

    working on dither effect

--- src/dither.ml
+++ src/dither.ml
@@ -1,7 +1,45 @@
 open Core
 
 (* This should look familiar by now! *)
-let transform image = image
+let transform image =
+  (* Image.mapi image ~f:(fun ~x ~y (r, g, b) -> ) *)
+  ignore image
+;;
+
+let add_error image x y =
+  if x + 1 < Image.width image
+  then (
+    let right_pixel = Image.get image ~x:(x + 1) ~y:(y + 1) in
+    Image.set
+      image
+      ~x:(x + 1)
+      ~y:(y + 1)
+      (Pixel.( + ) right_pixel (7 * 255 / 16, 7 * 255 / 16, 7 * 255 / 16)));
+  if x - 1 > 0 && y + 1 < Image.height image
+  then (
+    let left_pixel = Image.get image ~x:(x - 1) ~y:(y - 1) in
+    Image.set
+      image
+      ~x:(x - 1)
+      ~y:(y + 1)
+      (Pixel.( + ) left_pixel (3 * 255 / 16, 3 * 255 / 16, 3 * 255 / 16)));
+  if y + 1 < Image.height image
+  then (
+    let left_pixel = Image.get image ~x ~y:(y - 1) in
+    Image.set
+      image
+      ~x
+      ~y:(y + 1)
+      (Pixel.( + ) left_pixel (3 * 255 / 16, 3 * 255 / 16, 3 * 255 / 16)));
+  if y + 1 < Image.height image && x + 1 < Image.width image
+  then (
+    let se_pixel = Image.get image ~x:(x + 1) ~y:(y + 1) in
+    Image.set
+      image
+      ~x:(x + 1)
+      ~y:(y + 1)
+      (Pixel.( + ) se_pixel (255 / 16, 255 / 16, 255 / 16)))
+;;
 
 let command =
   Command.basic
@@ -17,5 +55,6 @@ let command =
         let image = Image.load_ppm ~filename |> transform in
         Image.save_ppm
           image
-          ~filename:(String.chop_suffix_exn filename ~suffix:".ppm" ^ "_dither.ppm")]
+          ~filename:
+            (String.chop_suffix_exn filename ~suffix:".ppm" ^ "_dither.ppm")]
 ;;
