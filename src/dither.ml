38c9de2d1e9933a7599de0e32665c1974d4cafc7
Sonica <sonicap2020@gmail.com>
2023-07-05 21:14:16 +0000

    Dither

--- src/dither.ml
+++ src/dither.ml
@@ -1,12 +1,6 @@
 open Core
 
-(* This should look familiar by now! *)
-let transform image =
-  (* Image.mapi image ~f:(fun ~x ~y (r, g, b) -> ) *)
-  ignore image
-;;
-
-let add_error image x y =
+let dist_error image x y error =
   if x + 1 < Image.width image
   then (
     let right_pixel = Image.get image ~x:(x + 1) ~y:(y + 1) in
@@ -14,23 +8,23 @@ let add_error image x y =
       image
       ~x:(x + 1)
       ~y:(y + 1)
-      (Pixel.( + ) right_pixel (7 * 255 / 16, 7 * 255 / 16, 7 * 255 / 16)));
+      (Pixel.( + ) right_pixel (7 * 255 / 16 * error, 7 * 255 / 16 * error, 7 * 255 / 16 * error)));
   if x - 1 > 0 && y + 1 < Image.height image
   then (
-    let left_pixel = Image.get image ~x:(x - 1) ~y:(y - 1) in
+    let sw_pixel = Image.get image ~x:(x - 1) ~y:(y - 1) in
     Image.set
       image
       ~x:(x - 1)
       ~y:(y + 1)
-      (Pixel.( + ) left_pixel (3 * 255 / 16, 3 * 255 / 16, 3 * 255 / 16)));
+      (Pixel.( + ) sw_pixel (3 * 255 / 16 * error, 3 * 255 / 16 * error, 3 * 255 / 16 * error)));
   if y + 1 < Image.height image
   then (
-    let left_pixel = Image.get image ~x ~y:(y - 1) in
+    let s = Image.get image ~x ~y:(y - 1) in
     Image.set
       image
       ~x
       ~y:(y + 1)
-      (Pixel.( + ) left_pixel (3 * 255 / 16, 3 * 255 / 16, 3 * 255 / 16)));
+      (Pixel.( + ) s (3 * 255 / 16 * error, 3 * 255 / 16 * error, 3 * 255 / 16 * error)));
   if y + 1 < Image.height image && x + 1 < Image.width image
   then (
     let se_pixel = Image.get image ~x:(x + 1) ~y:(y + 1) in
@@ -40,6 +34,13 @@ let add_error image x y =
       ~y:(y + 1)
       (Pixel.( + ) se_pixel (255 / 16, 255 / 16, 255 / 16)))
 ;;
+
+(* This should look familiar by now! *)
+let transform image =
+  Image.mapi image ~f:(fun ~x ~y (r, g, b) ->
+    if r >  255 / 2 then dist_error (image x y); (0, 0, 0) else 1, 1, 1)
+;;
+
 
 let command =
   Command.basic
