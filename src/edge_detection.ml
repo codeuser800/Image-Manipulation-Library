47da2be622315224e0e740ccc4f0179897970878
Sonica <sonicap2020@gmail.com>
2023-07-07 20:18:24 +0000

    fixed edge detection

--- src/edge_detection.ml
+++ src/edge_detection.ml
@@ -5,15 +5,24 @@ open Core
 
 let calc_horizontal_grad ~img_slice =
   let grad = [| [| -1; 0; 1 |]; [| -2; 0; 2 |]; [| -1; 0; 1 |] |] in
-  Image.foldi img_slice ~init:0 ~f:(fun ~x ~y sum (r, _g, _b) ->
-    Core.print_s [%message (Array.get (Array.get grad 1) 0 : int)];
-    sum + (Array.get (Array.get grad x) y * r))
+  let x =
+    Image.foldi img_slice ~init:0 ~f:(fun ~x ~y sum (r, _g, _b) ->
+      (* Core.print_s [%message "x" (r : int) (img_slice : Image.t)]; *)
+      sum + (Array.get (Array.get grad y) x * r))
+  in
+  (* Core.print_s [%message (x : int)]; *)
+  x
 ;;
 
 let calc_vertical_grad ~img_slice =
-  let grad = [| [| -1; -2; 1 |]; [| 0; 0; 0 |]; [| 1; 2; 1 |] |] in
-  Image.foldi img_slice ~init:0 ~f:(fun ~x ~y sum (r, _g, _b) ->
-    sum + (Array.get (Array.get grad x) y * r))
+  let grad = [| [| -1; -2; -1 |]; [| 0; 0; 0 |]; [| 1; 2; 1 |] |] in
+  let y =
+    Image.foldi img_slice ~init:0 ~f:(fun ~x ~y sum (r, _g, _b) ->
+      (* Core.print_s [%message "y" (r : int) (img_slice : Image.t)]; *)
+      sum + (Array.get (Array.get grad y) x * r))
+  in
+  (* Core.print_s [%message (y : int)]; *)
+  y
 ;;
 
 let transform image =
@@ -22,26 +31,28 @@ let transform image =
   let threshold = 0.4 *. float_of_int (Image.max_val image) in
   let max_value = Image.max_val image in
   Image.mapi image ~f:(fun ~x ~y (_r, _g, _b) ->
-    if x - 1 >= 0
-       && y - 1 >= 0
-       && x + 1 < Image.width image
-       && y + 1 < Image.height image
-    then (
-      let slice =
+    match
+      x > 0
+      && y > 0
+      && x + 1 < Image.width image
+      && y + 1 < Image.height image
+    with
+    | false -> 0, 0, 0
+    | true ->
+      let img_slice =
         Image.slice
           image
           ~x_start:(x - 1)
-          ~x_end:(x + 1)
+          ~x_end:(x + 2)
           ~y_start:(y - 1)
-          ~y_end:(y + 1)
+          ~y_end:(y + 2)
       in
-      let gx = calc_horizontal_grad ~img_slice:slice in
-      let gy = calc_vertical_grad ~img_slice:slice in
+      let gx = calc_horizontal_grad ~img_slice in
+      let gy = calc_vertical_grad ~img_slice in
       let g = sqrt (float_of_int (gx * gx) +. float_of_int (gy * gy)) in
-      if Float.( > ) g threshold
+      if Float.( < ) g threshold
       then 0, 0, 0
       else max_value, max_value, max_value)
-    else 0, 0, 0)
 ;;
 
 let command =
